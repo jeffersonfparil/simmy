cb6d2f2dcf40fb3629cfe62b85ae4430164e6673
jeffersonfparil <jeffersonparil@gmail.com>
2026-09-25 13:03:16 +1000

    dev: drafting Data initialiser

--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,13 @@
 use crate::linalg::context::GpuContext;
 use crate::linalg::tensor::GpuTensor;
-use anyhow::{Context, Result, ensure};
-use rand::prelude::*;
+use anyhow::Result;
 use rand_chacha::{ChaCha8Rng, rand_core::SeedableRng};
-use rand_distr::{Beta, Distribution, Uniform, Exp};
+use rand_distr::{Distribution, Exp};
+
+// use anyhow::{Context, Result, ensure};
+// use rand::prelude::*;
+// use rand_chacha::{ChaCha8Rng, rand_core::SeedableRng};
+// use rand_distr::{Beta, Distribution, Uniform, Exp};
 
 #[derive(Debug, Clone)]
 pub struct Chromosome {
@@ -17,7 +21,7 @@ pub struct Chromosome {
 #[derive(Debug, Clone)]
 pub struct Locus {
     pub chromosome_id: usize, // index of the chromosome containing this locus, which assumes one or more chromosomes are stored in a vector (contiguous/ordered list)
-    pub position: u64,      // position in the chromosome
+    pub position: u64,        // position in the chromosome
     pub alleles: Vec<String>, // sequence of each allele
     pub length: u64, // maximum size of alleles, i.e. the number of bases of the longest allele
     pub col_idx: Vec<u64>, // The column indices in the main genotype tensor, each referring to an allele
@@ -25,9 +29,9 @@ pub struct Locus {
 
 #[derive(Debug, Clone)]
 pub struct Trait {
-    name: String,
-    is_sex: bool,
-    description: String,
+    _name: String,
+    _is_sex: bool,
+    _description: String,
 }
 
 #[derive(Debug, Clone)]
@@ -51,19 +55,19 @@ pub struct Data {
 
 impl Data {
     pub fn new(
-        ctx: GpuContext,
+        _ctx: GpuContext,
         n_entries: usize,
         n_chromosomes: usize,
         n_loci: usize,
-        n_traits: usize,
-        with_sex: bool,
+        _n_traits: usize,
+        _with_sex: bool,
         seed: u64,
     ) -> Result<Self> {
         // Entries
         let mut entries: Vec<Entry> = Vec::with_capacity(n_entries);
         let n_digits: usize = format!("{}", n_entries).len();
         for i in 0..n_entries {
-            entries.push(Entry{
+            entries.push(Entry {
                 name: format!("entry_{:0>n_digits$}", i),
                 species: "".to_owned(),
                 ploidy: 2,
@@ -75,7 +79,7 @@ impl Data {
         let mut genome: Vec<Chromosome> = Vec::with_capacity(n_chromosomes);
         let n_digits: usize = format!("{}", n_chromosomes).len();
         for i in 0..n_chromosomes {
-            genome.push(Chromosome{
+            genome.push(Chromosome {
                 name: format!("chromosome_{:0>n_digits$}", i),
                 lengths: (1_000_000, 1_000_000),
                 centromere_positions: (500_000, 500_000),
@@ -89,13 +93,13 @@ impl Data {
         // Divy up the loci into chromosomes
         let mut positions_per_chromosome: Vec<Vec<u64>> = Vec::with_capacity(n_chromosomes);
         let mut m = (n_loci / n_chromosomes) as u64;
-        for i in 0..n_chromosomes {
+        for (i, chromosome) in genome.iter().enumerate() {
             m += if i < (n_chromosomes - 1) {
                 0
             } else {
                 (n_loci as u64) - (m * n_chromosomes as u64)
             };
-            let n = genome[i].lengths.0;
+            let n = chromosome.lengths.0;
             let k = n / m;
             let mut pos: Vec<u64> = (0..n).step_by(k as usize).collect();
             if (pos.len() as u64) < m {
@@ -106,21 +110,26 @@ impl Data {
         let exponential = Exp::new(1.0).expect("Failed to initialise exponential distribution!");
         let mut loci: Vec<Locus> = Vec::with_capacity(n_loci);
         let mut locus_allele_counter: u64 = 0;
-        for i in 0..n_chromosomes {
-            for &pos in &positions_per_chromosome[i] {
-                let n_alleles = (exponential.sample(&mut rng) as f64).round().max(2.0) as usize;
-                loci.push(Locus{
+        for (i, positions) in positions_per_chromosome.iter().enumerate() {
+            for &pos in positions {
+                let n_alleles = {
+                    let a: f64 = exponential.sample(&mut rng);
+                    a.round().max(2.0) as usize
+                };
+                loci.push(Locus {
                     chromosome_id: i,
                     position: pos,
-                    alleles: ["A", "T", "C", "G", "D"][0..n_alleles].iter().map(|&x| x.to_owned()).collect::<Vec<String>>(),
+                    alleles: ["A", "T", "C", "G", "D"][0..n_alleles]
+                        .iter()
+                        .map(|&x| x.to_owned())
+                        .collect::<Vec<String>>(),
                     length: 1,
-                    col_idx: (locus_allele_counter..(locus_allele_counter+(n_alleles as u64))).collect::<Vec<u64>>(),
+                    col_idx: (locus_allele_counter..(locus_allele_counter + (n_alleles as u64)))
+                        .collect::<Vec<u64>>(),
                 });
                 locus_allele_counter += n_alleles as u64;
             }
         }
-
-
         todo!()
     }
 }
