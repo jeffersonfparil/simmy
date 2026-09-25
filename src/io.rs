8194a75e0908666661f26530c7038e6eae8fcac4
jeffersonfparil <jeffersonparil@gmail.com>
2026-09-25 18:29:19 +1000

    dev: drafting Data initialiser

--- src/io.rs
+++ src/io.rs
@@ -7,9 +7,9 @@ use rand_distr::{Beta, Distribution, Exp, Normal};
 #[derive(Debug, Clone)]
 pub struct Chromosome {
     pub name: String,
-    pub lengths: (usize, usize),
-    pub centromere_positions: (usize, usize),
-    pub ld_decay_distance: usize,
+    pub lengths: (usize, usize), // homologous chromosome lengths
+    pub centromere_positions: (usize, usize), // centromere positions in corresponding homologous chromosomes
+    pub ld_decay_distance: usize, // will be used in mating assuming r(d) = exp(-d/L), where d is the distance between a pair of loci in bases and L is ld_decay_distance.
     pub is_sex_chromosome: bool,
 }
 
@@ -44,7 +44,7 @@ pub struct Data {
     pub genome: Vec<Chromosome>,
     pub loci: Vec<Locus>,
     pub traits: Vec<Trait>,
-    pub genotype_data: GpuTensor,
+    pub genotype_data: GpuTensor, // shape: n_entries x n_loci_alleles x maternal+paternal haplotypes
     pub phenotype_data: GpuTensor,
 }
 
@@ -77,6 +77,10 @@ impl Data {
             "The number of traits need to be greater than zero!"
         );
         ensure!(ploidy > 0, "Ploidy need to be greater than zero!");
+        ensure!(
+            ploidy.is_multiple_of(2),
+            "We do not support odd ploidies at the moment!"
+        );
         // Entries
         let mut entries: Vec<Entry> = Vec::with_capacity(n_entries);
         let n_digits: usize = format!("{}", n_entries).len();
@@ -98,7 +102,7 @@ impl Data {
                 lengths: (1_000_000, 1_000_000),
                 centromere_positions: (500_000, 500_000),
                 ld_decay_distance: 10_000,
-                is_sex_chromosome: i < (n_chromosomes - 1),
+                is_sex_chromosome: with_sex && (i == (n_chromosomes - 1)),
             });
         }
         // Loci
@@ -110,13 +114,13 @@ impl Data {
             m += if i < (n_chromosomes - 1) {
                 0
             } else {
+                // Move remaining loci to the last chromosome for simplicity
                 n_loci - (m * n_chromosomes)
             };
             let n = chromosome.lengths.0;
-            let k = n / m;
-            let mut pos: Vec<usize> = (0..n).step_by(k).collect();
-            if pos.len() < m {
-                pos.push(n - 1);
+            let mut pos: Vec<usize> = Vec::with_capacity(m);
+            for j in 0..m {
+                pos.push(j * n / m);
             }
             positions_per_chromosome.push(pos);
         }
@@ -129,7 +133,7 @@ impl Data {
                 let n_alleles = {
                     let a: f64 = exponential.sample(&mut rng);
                     a.round().clamp(2.0, 5.0) as usize
-                }; // TODO: add more alleles but for now we only have SNPs, i.e. A, T, C, G, and D
+                }; // TODO: add more alleles but for now we only have SNPs, i.e. A, T, C, G, and D (note that these are still SNPs even though we can have multi-allelic loci because the variations remain single nucleotides)
                 loci.push(Locus {
                     chromosome_id: i,
                     position: pos,
@@ -155,14 +159,25 @@ impl Data {
         }
         // Genotype data
         let mut genotype_data_tmp: Vec<f32> = vec![0.0; n_entries * n_loci_alleles * 2];
-        let beta =
-            Beta::new(0.5, 0.5).expect("Failed to initialise a Beta distribution (a=b=0.5)!");
-        for _i in 0..n_entries {
-            for _j in 0..n_loci_alleles {
-                let n_alleles_from_parent_1 = (beta.sample(&mut rng) * (ploidy as f32)).round();
-                let n_alleles_from_parent_2 = (beta.sample(&mut rng) * (ploidy as f32)).round();
-                genotype_data_tmp.push(n_alleles_from_parent_1);
-                genotype_data_tmp.push(n_alleles_from_parent_2);
+        let beta_n =
+            Beta::new(2.0, 5.0).expect("Failed to initialise a Beta distribution (a=2; b=5)!"); // We will sample allele indices per locus from a skewed distribution, i.e. biased towards the first allele which is more realistic than uniform distribution.
+        let beta_u =
+            Beta::new(0.5, 0.5).expect("Failed to initialise a Beta distribution (a=b=0.5)!"); // We will sample from a realistic U shaped distribution (bell-shaped assumes balancing selection on all loci which rarely occur in genome-wide loci).
+        for i in 0..n_entries {
+            for locus in &loci {
+                let allele_dosage_from_parent_1 =
+                    (beta_u.sample(&mut rng) * ((ploidy / 2) as f32)).round();
+                let allele_dosage_from_parent_2 =
+                    ((ploidy / 2) as f32) - allele_dosage_from_parent_1;
+                let idx = i * 2 * n_loci_alleles;
+                // We select an allele from each parent, where we may have multiple alleles per locus, hence parent1 ∈ {0,1} & parent2 ∈ {1,0} at a single locus is valid for a diploid because allele count sum to 2.
+                let n_alleles = locus.col_idx.len();
+                let j_1 = locus.col_idx
+                    [(beta_n.sample(&mut rng) * ((n_alleles - 1) as f32)).round() as usize];
+                let j_2 = locus.col_idx
+                    [(beta_n.sample(&mut rng) * ((n_alleles - 1) as f32)).round() as usize];
+                genotype_data_tmp[idx + (2 * j_1)] = allele_dosage_from_parent_1;
+                genotype_data_tmp[idx + (2 * j_2) + 1] = allele_dosage_from_parent_2;
             }
         }
         let genotype_data = GpuTensor::from_f32(
@@ -176,18 +191,19 @@ impl Data {
         let mut phenotype_data_tmp: Vec<f32> = vec![0.0; n_entries * n_traits];
         let normal =
             Normal::new(0.0, 1.0).expect("Failed to initialise a standard normal distribution!");
-        for _i in 0..n_entries {
-            for _j in 0..n_traits {
-                phenotype_data_tmp.push(normal.sample(&mut rng));
+        for i in 0..n_entries {
+            for j in 0..n_traits {
+                phenotype_data_tmp[(i * n_traits) + j] = normal.sample(&mut rng);
             }
         }
         let phenotype_data = GpuTensor::from_f32(
             ctx,
             &phenotype_data_tmp,
-            &[n_entries as u32, n_loci_alleles as u32, 2],
+            &[n_entries as u32, n_traits as u32],
             None,
             None,
         )?;
+        // Output
         Ok(Self {
             entries,
             genome,
@@ -197,4 +213,6 @@ impl Data {
             phenotype_data,
         })
     }
+    // TODO: pmating pair selection
+    // TODO: mating with LD
 }
